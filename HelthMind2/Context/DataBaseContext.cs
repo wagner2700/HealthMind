13174f1160ae144bf7173f64ce152b882bb52d21
wagner2700 <waguinhoxd4@gmail.com>
2023-04-28 20:29:13 -0300

    first

---
+++ HelthMind2/Context/DataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using HelthMind2.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelthMind2.Context
+{
+    public class DataBaseContext: DbContext
+    {
+        
+        // Propriedade responsável pelo acesso Medico
+        public DbSet<MedicoModel> medico { get; set; }
+        public DbSet<PacienteModel> paciente { get; set; }
+
+        // Propriedade responsável pelo acesso Paciente
+        //public DbSet<PacienteModel> paciente { get; set; }
+
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DataBaseContext()
+        {
+        }
+    }
+}
---
+++ HelthMind2/Model/PacienteModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace HelthMind2.Model
+{
+    [Table("PACIENTE")]
+    public class PacienteModel
+    {
+      
+        
+
+            [Key]
+            [Required]
+            [Column("PACIENTE ID")]
+            public int PacienteId { get; set; }
+
+            [Column("NOME")]
+            [Required(ErrorMessage = "Nome é obrigatório")]
+            public string Nome { get; set; }
+
+            [Column("CPF")]
+            [Required(ErrorMessage = "CPF é obrigatório")]
+            public string CpfPaciente { get; set; }
+
+
+            [Column("ESTADO")]
+            [Required]
+            public string Estado { get; set; }
+
+        public PacienteModel(int pacienteId, string nome, string cpfPaciente, string estado)
+        {
+            PacienteId = pacienteId;
+            Nome = nome;
+            CpfPaciente = cpfPaciente;
+            Estado = estado;
+        }
+    }
+    }
+
